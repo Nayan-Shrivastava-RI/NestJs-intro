5a99929b1ef2e9a1ba7800be8e8234addcf48ee2
Nayan-Shrivastava <nayanshrivastava800@gmail.com>
2021-07-07 19:07:40 +0530

    added User module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb://localhost/authexample') ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.interface';
+import { CreateUserDto } from './dto/create-users.dto';
+
+@Injectable()
+export class usersService{
+
+    constructor(@InjectModel('User') private userModel : Model<User>){ }
+
+    async create(createUserDto : CreateUserDto) : Promise<User>{
+        let createdUser = new this.userModel(createUserDto);
+        return await createdUser.save();
+    }
+
+
+    async findOneByEmail(email): Promise<User>{
+        return await this.userModel.findOne({email: email});
+    }
+
+
+}
